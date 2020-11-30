a1a4c06cb48345c57bc51282a19e47e53807cdeb
perterxkl <18711345569@163.com>
2020-11-30 20:23:29 +0800

    update config and pom.xml for cicd

---
+++ src/test/java/com/ita/domain/service/BoxServiceTest.java
@@ -0,0 +1,14 @@
+package com.ita.domain.service;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Dillon Xie
+ * @date 11/30/2020
+ */
+public class BoxServiceTest {
+    @Test
+    public void should_when_given() {
+        System.out.println("sucess");
+    }
+}
