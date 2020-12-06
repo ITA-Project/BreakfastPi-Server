da56fb803bae0d22d2832a1a366fc51d71db5659
perterxkl <18711345569@163.com>
2020-12-06 10:41:14 +0800

    test for jenkins

--- src/main/java/com/ita/domain/controller/TestController.java
+++ src/main/java/com/ita/domain/controller/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
     private static final String SUCCESS = "SUCCESS";
 
     @GetMapping("test")
