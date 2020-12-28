a96d5572ab0414910acc170be1262967683b6bcd
Felix Wang <felix.wang@oocl.com>
2020-12-28 14:29:57 +0800

    Revised FTP config and api

--- src/main/java/com/ita/domain/controller/FtpController.java
+++ src/main/java/com/ita/domain/controller/FtpController.java
@@ -2,11 +2,13 @@ package com.ita.domain.controller;
 
 import com.ita.domain.service.FtpService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/ftp")
+@RestController
+@RequestMapping("/ftp")
 public class FtpController {
 
     private final FtpService ftpServiceImpl;
