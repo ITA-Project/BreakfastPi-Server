5dfe94e1de2596a3e01db6dc44d3430020d4ff59
YUANSH3 <yuansh3@oocl.com>
2020-12-28 11:44:27 +0800

    aes secret

--- src/main/java/com/ita/domain/service/impl/MqttMessageServiceImpl.java
+++ src/main/java/com/ita/domain/service/impl/MqttMessageServiceImpl.java
@@ -2,12 +2,14 @@ package com.ita.domain.service.impl;
 
 import com.ita.domain.config.MqttConfig;
 import com.ita.domain.service.MessageService;
+import com.ita.utils.AESUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +18,8 @@ public class MqttMessageServiceImpl implements MessageService {
 
     @Autowired
     private MqttConfig mqttConfig;
+    @Value("${aes.secret}")
+    private String secret;
 
     @Override
     public void send(String message) {
@@ -25,8 +29,9 @@ public class MqttMessageServiceImpl implements MessageService {
             MqttConnectOptions connOpts = new MqttConnectOptions();
 
             connOpts.setCleanSession(false);
-            connOpts.setUserName(mqttConfig.getUsername());
-            connOpts.setPassword(mqttConfig.getPassword().toCharArray());
+            String content = AESUtil.decrypt(mqttConfig.getConnStr(), secret);
+            connOpts.setUserName(content.split("/")[0]);
+            connOpts.setPassword(content.split("/")[1].toCharArray());
             sampleClient.connect(connOpts);
 
             MqttMessage mqttMessage = new MqttMessage(message.getBytes());
