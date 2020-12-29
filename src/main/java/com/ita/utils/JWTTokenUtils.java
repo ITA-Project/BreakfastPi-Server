94597a058663ed6c62968c61355178222ef4b796
ZHANGKH <Khai.zhang@oocl.com>
2020-12-29 15:21:53 +0800

    [Khai] add JWTTokenUtils

---
+++ src/main/java/com/ita/utils/JWTTokenUtils.java
@@ -0,0 +1,22 @@
+package com.ita.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ita.domain.entity.User;
+import java.util.Date;
+
+public class JWTTokenUtils {
+
+  private final static String secret = "BA125SDSR62WQEQ23126";
+  public final static String AUTHORIZATION = "Authorization";
+
+  public static String getUserToken(User user) {
+    return JWT.create()
+        .withKeyId(user.getId().toString())
+        .withAudience(user.getId().toString())
+        .withClaim("userId", user.getId())
+        .withClaim("time", new Date())
+        .sign(Algorithm.HMAC256(secret));
+  }
+
+}
