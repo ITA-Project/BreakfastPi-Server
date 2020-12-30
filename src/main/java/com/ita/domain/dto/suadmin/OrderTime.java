3b700e5f2f56daffec9cde353a577d543e27e21e
YUANSH3 <yuansh3@oocl.com>
2020-12-30 14:08:59 +0800

    order time statistics

--- src/main/java/com/ita/domain/dto/suadmin/OrderTime.java
+++ src/main/java/com/ita/domain/dto/suadmin/OrderTime.java
@@ -1,15 +1,16 @@
 package com.ita.domain.dto.suadmin;
 
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-class OrderTime {
+public class OrderTime {
   private List<Long> data;
 }
--- src/main/java/com/ita/domain/mapper/OrderMapper.java
+++ src/main/java/com/ita/domain/mapper/OrderMapper.java
@@ -38,4 +38,6 @@ public interface OrderMapper {
     Long selectOrdersByProductIdAndShopAndPeriodTime(OrderQuery orderQuery);
 
     List<OrderSaleDTO> selectSaleByShopAndPeriodTime(OrderQuery orderQuery);
+
+    List<Order> selectOrderByShopAndPeriodTime(OrderQuery orderQuery);
 }
