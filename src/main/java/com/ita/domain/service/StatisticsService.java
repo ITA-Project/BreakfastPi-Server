052eac0c8f71c6310ed169cd5f13b0381909fe28
YUANSH3 <yuansh3@oocl.com>
2020-12-31 10:44:51 +0800

    exception

--- src/main/java/com/ita/domain/service/StatisticsService.java
+++ src/main/java/com/ita/domain/service/StatisticsService.java
@@ -1,7 +1,8 @@
 package com.ita.domain.service;
 
 import com.ita.domain.dto.suadmin.SaleDataDTO;
+import com.ita.domain.error.BusinessException;
 
 public interface StatisticsService {
-  SaleDataDTO generateStatisticsData(Integer shopId, String type);
+  SaleDataDTO generateStatisticsData(Integer shopId, String type) throws BusinessException;
 }
